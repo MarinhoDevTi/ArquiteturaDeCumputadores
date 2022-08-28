07b59d1c293e877b58ea513a44156f17ed6c1e45
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-28 19:16:03 -0300

    Update-Arduino

--- arduino/display7Segmentos/display7Segmentos.ino
+++ arduino/display7Segmentos/display7Segmentos.ino
@@ -1,5 +1,15 @@
+// Codificador-Binário-Decimal 8 - 1 - L
+// (Display) 7 Segmentos
+
+boolean A = 0;
+boolean B = 0;
+boolean C = 0;
+boolean D = 0;
 void setup() {
   // put your setup code here, to run once:
+  pinMode(13,OUTPUT); //Sa
+  pinMode(12,OUTPUT); //Sb
+  
 
 }
 
