12aa5485cc3421944b188d0adf808c6f1d88a12c
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-30 11:45:17 -0300

    Update display7Segmentos.ino

--- arduino/display7Segmentos/display7Segmentos.ino
+++ arduino/display7Segmentos/display7Segmentos.ino
@@ -9,11 +9,37 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(13,OUTPUT); //Sa
   pinMode(12,OUTPUT); //Sb
-  
+  pinMode(11,OUTPUT); //Sc 
+  pinMode(10,OUTPUT); //Sd 
+  pinMode(9,OUTPUT); //Se 
+  pinMode(8,OUTPUT); //Sf
+  pinMode(7,OUTPUT); //Sg 
+  //pinMode(3,IMPUT);  //entrada D 
+  //pinMode(4,IMPUT); //entrada C 
+  //pinMode(5,IMPUT); //entrada B 
+  //pinMode(6,IMPUT); //entrada A   
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  A = digitalRead(6);
+  B = digitalRead(5);
+  C = digitalRead(4);
+  D = digitalRead(3);
 
 }
+
+// Modulo L com 1111
+if (A*B*C*D == 1){
+  digitalWrite(13,0);
+  digitalWrite(12,0);
+  digitalWrite(11,0);
+  digitalWrite(10,1);
+  digitalWrite(9,1);
+  digitalWrite(8,1);
+  digitalWrite(7,0);
+}
+
+
+
