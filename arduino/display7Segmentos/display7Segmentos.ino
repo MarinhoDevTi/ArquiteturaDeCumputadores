db617cff3082a26d69c82e7c93b6f13abba7f9bb
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-30 12:01:46 -0300

    Update display7Segmentos.ino

--- arduino/display7Segmentos/display7Segmentos.ino
+++ arduino/display7Segmentos/display7Segmentos.ino
@@ -41,5 +41,30 @@ if (A*B*C*D == 1){
   digitalWrite(7,0);
 }
 
+//MODULO um com 0001
+if (!A * !B * !C D == 1){
+  digitalWrite(13,0);
+  digitalWrite(12,1);
+  digitalWrite(11,1);
+  digitalWrite(10,0);
+  digitalWrite(9,0);
+  digitalWrite(8,0);
+  digitalWrite(7,0);
+
+}
+
+//MODULO oito com 1000
+if (A * !B * !C * !D  == 1){
+  digitalWrite(13,1);
+  digitalWrite(12,1);
+  digitalWrite(11,1);
+  digitalWrite(10,1);
+  digitalWrite(9,1);
+  digitalWrite(8,1);
+  digitalWrite(7,1);
+
+}
+
+
 
 
