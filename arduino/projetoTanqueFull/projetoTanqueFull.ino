3665a4f78318e6f64819dbf667ccbfacf0fd6419
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-09-02 20:38:44 -0300

    Update projetoTanqueFull.ino

--- arduino/projetoTanqueFull/projetoTanqueFull.ino
+++ arduino/projetoTanqueFull/projetoTanqueFull.ino
@@ -11,3 +11,17 @@ void void setup()
   pinMode(7, OUTPUT); // Cheio
   Serial.begin(9600);
 }
+
+void void loop()
+{
+  tanque = analogRead(A0); // Lê entrada analógica A0
+  Serial.print(" - tanque: "); //Imprime texto
+  Serial.println(tanque); // Imprime variável tanque no monitor 
+
+  // Led vazio
+  if (tanque < 255 and tanque > 2){
+    digitalWrite(3,1); //Liga Led Vazio laranja
+  } {
+
+  }
+}
