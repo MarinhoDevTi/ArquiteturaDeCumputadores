d0fbfc290a0bd59815e3a5605af594927653153c
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-09-03 17:20:37 -0300

    Update projetoTanqueFull.ino

--- arduino/projetoTanqueFull/projetoTanqueFull.ino
+++ arduino/projetoTanqueFull/projetoTanqueFull.ino
@@ -1,113 +1,58 @@
-//conversor Analógico digital;
+// conversor Analógico Digital;
 int tanque = 0;
-
 void setup()
 {
   pinMode(A0, INPUT);
-  pinMode(3, OUTPUT); //vazio
-  pinMode(4, OUTPUT); // 1/4
-  pinMode(5, OUTPUT); //metade      //tanque < 255 and tanque > 2
-  pinMode(6, OUTPUT); // 3/4
-  pinMode(7, OUTPUT); // Cheio
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  pinMode(3, OUTPUT);//vazio
+  pinMode(4, OUTPUT);//1/4
+  pinMode(5, OUTPUT);//metade
+  pinMode(6, OUTPUT);//3/4
+  pinMode(7, OUTPUT);//cheio
   Serial.begin(9600);
 }
-
 void loop()
 {
-  tanque = analogRead(A0); // Lê entrada analógica A0
+  tanque = analogRead(A0); //Lê entrada analógica A0
   Serial.print(" - tanque: "); //Imprime texto
-  Serial.println(tanque); // Imprime variável tanque no monitor 
-
+  Serial.println(tanque); //imprime variável tanque no monitor 
   // Led vazio
-  while (tanque < 4 ){
-    digitalWrite(3,1); //Liga Led Vazio    
-  } 
-  digitalWrite(3,0)
-  
-  while (tanque > 4 and tanque <= 130) {
-    digitalWrite(4,1);
-  } 
-  digitalWrite(4,0)
-
-  while (tanque > 130 and tanque <= 260) {
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-  } 
-
-  digitalWrite(4,0);
-  digitalWrite(5,0);
-
-  while (tanque > 260 and tanque <= 400) {
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-    digitalWrite(6,1);
+  if (tanque<127 and tanque>2){
+    digitalWrite(3,1); //Liga LEd vaizio Laranja
+  }else{digitalWrite(3,0); //Desliga Led metade Laranja
   }
-
-  digitalWrite(4,0);
-  digitalWrite(5,0);
-  digitalWrite(6,0);
-
-  while (tanque > 400 and tanque <= 560) {
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-    digitalWrite(6,1);
-    digitalWrite(7,1);
-  } 
-
-  digitalWrite(4,0);
-  digitalWrite(5,0);
-  digitalWrite(6,0);
-  digitalWrite(7,0);
-  
-  while (tanque > 560 and tanque <= 680) {
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-    digitalWrite(6,1);
-    digitalWrite(7,1);
-    digitalWrite(8,1);
-  } 
-
-  digitalWrite(4,0);
-  digitalWrite(5,0);
-  digitalWrite(6,0);
-  digitalWrite(7,0);
-  digitalWrite(8,0);   
-  
-  while (tanque > 680 and tanque <= 780) {
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-    digitalWrite(6,1);
-    digitalWrite(7,1);
-    digitalWrite(8,1);
-    digitalWrite(9,1);
-  } 
-  
-  digitalWrite(4,0);
-  digitalWrite(5,0);
-  digitalWrite(6,0);
-  digitalWrite(7,0);
-  digitalWrite(8,0);
-  digitalWrite(9,0);
-
-  while (tanque > 780 and tanque <= 1023) {
-    digitalWrite(4,1);
-    digitalWrite(5,1);
-    digitalWrite(6,1);
-    digitalWrite(7,1);
-    digitalWrite(8,1);
-    digitalWrite(9,1);
-    digitalWrite(10,1);
-    
+  //Led 1/4
+  if (tanque>=128){
+    digitalWrite(4,1); // Liga Led 1/4 verde
+  }else{digitalWrite(4,0); //Desliga Led metade verde
   }
-  digitalWrite(4,0);
-  digitalWrite(5,0);
-  digitalWrite(6,0);
-  digitalWrite(7,0);
-  digitalWrite(8,0);
-  digitalWrite(9,0);
-  digitalWrite(10,0);
-
+  //Led 3/4
+  if (tanque>=256){
+    digitalWrite(5,1); //Liga Led metade verde
+  }else{digitalWrite(5,0); //Desliga Led metade verde
+  }
+  //Led 3/4
+  if (tanque>=384){
+    digitalWrite(6,1); // Liga Led 3/4 verde
+  }else{digitalWrite(6,0); // Desliga Led 3/4 verde
+  }
+  //Led cheio
+  if (tanque>=512){
+    digitalWrite(7,1); //Liga Led cheio verde
+  }else{digitalWrite(7,0); // Desliga Led cheio verde}
+  }
+  //
+  if (tanque>=640){
+    digitalWrite(8,1); //Liga Led cheio verde
+  }else{digitalWrite(8,0); // Desliga Led cheio verde}
+  }
+  //
+  if (tanque>=768){
+    digitalWrite(9,1); //Liga Led cheio verde
+  }else{digitalWrite(9,0); // Desliga Led cheio verde}
+  }
+  //
+  if (tanque>1021 and tanque<=1024){
+    digitalWrite(10,1); //Liga Led cheio verde
+  }else{digitalWrite(10,0); // Desliga Led cheio verde}
+  }
 }
